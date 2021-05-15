e47dc864353bbf0cb2ffcc25b2954cc291da72ed
jordipg37 <jordipuig37@gmail.com>
2021-05-15 23:25:58 +0200

    added mmistakes template

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "My Birthday",
+        "excerpt":" today is my birthday       maybe i could do all the posts in markdown so it's easier to write. ","categories": [],
+        "tags": [],
+        "url": "/my-birthday/",
+        "teaser": null
+      },{
+        "title": "Testing Posts",
+        "excerpt":"this is a post from 1st may Today I'm starting to create my personal website. As stated, I don't know to write code in html but I'm ready to learn. This is a test so it is most likely nobody ever sees this. I hope this looks good in the...","categories": [],
+        "tags": [],
+        "url": "/testing-posts/",
+        "teaser": null
+      },{
+        "title": "First Entry",
+        "excerpt":" I don't really know html       Today I'm starting to create my personal website. As stated, I don't know to write code in html but I'm ready to learn.     This is a test so it is most likely nobody ever sees this. I hope this looks good in the webpage. ","categories": [],
+        "tags": [],
+        "url": "/first-entry/",
+        "teaser": null
+      }]
